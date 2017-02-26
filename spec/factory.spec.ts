1008809ef163d9b941479073f6574c9d086d118f
Will Pleasant-Ryan <will.ryan@atomicobject.com>
2017-02-25 20:38:32 -0500

    builder extension and derived values

--- spec/factory.spec.ts
+++ spec/factory.spec.ts
@@ -13,7 +13,7 @@ interface ChildType {
   grade: number
 }
 
-describe('factories do stuff', () => {
+describe('factories build stuff', () => {
   const childFactory = Factory.makeFactory<ChildType>({
     name: 'Kid',
     grade: 1
@@ -25,20 +25,20 @@ describe('factories do stuff', () => {
     spouse: null
   });
   it('makes an object from a factory', () => {
-    const jimmy = childFactory({ name: 'Jimmy' });
+    const jimmy = childFactory.build({ name: 'Jimmy' });
     expect(jimmy.name).to.eq('Jimmy');
     expect(jimmy.grade).to.eq(1);
   });
   it('can make use of sequence #', () => {
-    const susan = parentFactory({ name: 'Susan' });
-    const edward = parentFactory({ name: 'Edward' });
+    const susan = parentFactory.build({ name: 'Susan' });
+    const edward = parentFactory.build({ name: 'Edward' });
     expect(susan.birthday.getTime()).to.eq(new Date('2017/05/01').getTime());
     expect(edward.birthday.getTime()).to.eq(new Date('2017/05/02').getTime());
   });
   it('can handle has many', () => {
-    const jimmy = childFactory({ name: 'Jimmy' });
-    const alice = childFactory({ name: 'Alice', grade: 3 });
-    const susan = parentFactory({ name: 'Susan', children: [ jimmy, alice ] });
+    const jimmy = childFactory.build({ name: 'Jimmy' });
+    const alice = childFactory.build({ name: 'Alice', grade: 3 });
+    const susan = parentFactory.build({ name: 'Susan', children: [ jimmy, alice ] });
     expect(susan.children.map(c => c.name)).to.deep.eq(['Jimmy', 'Alice']);
   });
   it('can refer to other factories', () => {
@@ -46,13 +46,37 @@ describe('factories do stuff', () => {
       name: 'Timothy',
       birthday: Factory.each(i => new Date(`2017/05/${i}`)),
       children: Factory.each(() => [
-        childFactory({ name: 'Bobby' }),
-        childFactory({ name: 'Jane' })
+        childFactory.build({ name: 'Bobby' }),
+        childFactory.build({ name: 'Jane' })
       ]),
       spouse: null
     });
-    const tim = parentWithKidsFactory({ birthday: new Date('2017-02-01')});
+    const tim = parentWithKidsFactory.build({ birthday: new Date('2017-02-01')});
     expect(tim.children.map(c => c.name)).to.deep.eq(['Bobby', 'Jane']);
   });
-})
+  it('can extend existing factories', () => {
+    const geniusFactory = childFactory.extend({
+      grade: Factory.each(i => i*2)
+    });
+    const colin = geniusFactory.build({name: "Colin"});
+    expect(colin.grade).to.eq(2);
+    const albert = geniusFactory.build({name: "Albert"});
+    expect(albert.grade).to.eq(4);
+  });
+  it('can derive one value based on another value', () => {
+    interface Person {
+      readonly firstName : string
+      readonly lastName : string
+      readonly fullName : string
+    }
+    const personFactory = Factory.makeFactory<Person>({
+      firstName: '',
+      lastName: 'Bond',
+      fullName: ''
+    }).withDerivation('fullName', (p) => `${p.firstName} ${p.lastName}`);
+    //.withDerivation2(['firstName','lastName'],'fullName', (fn, ln) => `${fn} ${ln}`);
+    const bond = personFactory.build({firstName: 'James'});
+    expect(bond.fullName).to.eq('James Bond');
+  });
+});
 
--- src/factory.ts
+++ src/factory.ts
@@ -1,42 +1,133 @@
 export type FactoryFunc<T> = (item : Partial<T>) => T
 
-class Fact<T> {
-  constructor( readonly func: (seq: number) => T) {
+class Gen<T> {
+  constructor(readonly func: (seq: number) => T) {
   }
   public build(seq : number) : T
   {
     return this.func(seq);
   }
 }
+class Derived<TOwner,TProperty> {
+  constructor(readonly func: (owner: TOwner, seq: number) => TProperty) { }
+  public build(owner : TOwner, seq: number) : TProperty
+  {
+    const ret = this.func(owner, seq);
+    return ret;
+  }
+}
+
+class Factory<T> {
+  private seqNum: number;
+  constructor(readonly builder: Builder<T>) {
+    this.seqNum = 0;
+  }
+
+  public build(item: Partial<T>) : T
+  {
+    this.seqNum++;
+    const base = buildBase(this.seqNum, this.builder);
+    const v = Object.assign({}, base.value, item);
+    const keys = Object.keys(item);
+    for (const der of base.derived) {
+      if (keys.indexOf(der.key) < 0) {
+        (v as any)[der.key] = (der.derived as any).build(v, this.seqNum);
+      }
+    }
+    return v;
+
+  }
+
+  public extend(def: Partial<Builder<T>>) : Factory<T> {
+    const builder = Object.assign({}, this.builder, def);
+    return new Factory(builder);
+  }
+
+  public withDerivation<KOut extends keyof T>(kOut: KOut, f: (v1: T, seq: number) => T[KOut]) : Factory<T> {
+    const partial : any = {};
+    partial[kOut] = new Derived<T,T[KOut]>(f);
+    return this.extend(partial);
+  }
+
+  public withDerivation1<K1 extends keyof T, KOut extends keyof T >(
+      kInput: [K1], kOut: KOut, f: (v1: T[K1], seq: number) => T[KOut]) : Factory<T> {
+    const partial : any = {};
+    partial[kOut] = new Derived<T,T[KOut]>((t, i) => f(t[kInput[0]], i));
+    return this.extend(partial);
+  }
+
+  public withDerivation2<K1 extends keyof T, K2 extends keyof T, KOut extends keyof T>(
+      kInput: [K1, K2], kOut: KOut, f: (v1: T[K1], v2: T[K2], seq: number) => T[KOut]) : Factory<T>  {
+    const partial : any = {};
+    partial[kOut] = new Derived<T,T[KOut]>((t, i) => f(t[kInput[0]],t[kInput[1]], i));
+    return this.extend(partial);
+  }
+
+  public withDerivation3<K1 extends keyof T, K2 extends keyof T, K3 extends keyof T, KOut extends keyof T>(
+      kInput:[K1, K2, K3], kOut: KOut, f: (v1: T[K1], v2: T[K2], v3: T[K3], seq: number) => T[KOut]) : Factory<T> {
+    const partial : any = {};
+    partial[kOut] = new Derived<T,T[KOut]>((t, i) => f(t[kInput[0]],t[kInput[1]], t[kInput[2]], i));
+    return this.extend(partial);
+  }
+
+  public withDerivation4<K1 extends keyof T, K2 extends keyof T, K3 extends keyof T, K4 extends keyof T, KOut extends keyof T>(
+      kInput:[K1, K2, K3, K4], kOut: KOut, f: (v1: T[K1], v2: T[K2], v3: T[K3], v4: T[K4], seq: number) => T[KOut]) : Factory<T> {
+    const partial : any = {};
+    partial[kOut] = new Derived<T,T[KOut]>((t, i) => f(t[kInput[0]],t[kInput[1]], t[kInput[2]],t[kInput[3]], i));
+    return this.extend(partial);
+  }
+
+  public withDerivation5<K1 extends keyof T, K2 extends keyof T, K3 extends keyof T, K4 extends keyof T, K5 extends keyof T, KOut extends keyof T>(
+      kInput:[K1, K2, K3, K4, K5], kOut: KOut, f: (v1: T[K1], v2: T[K2], v3: T[K3], v4: T[K4], v5: T[K5], seq: number) => T[KOut]) : Factory<T> {
+    const partial : any = {};
+    partial[kOut] = new Derived<T,T[KOut]>((t, i) => f(t[kInput[0]],t[kInput[1]], t[kInput[2]],t[kInput[3]], t[kInput[4]], i));
+    return this.extend(partial);
+  }
+
+}
 
 type Builder<T> = {
-  [P in keyof T]: T[P] | Fact<T[P]>;
+  [P in keyof T]: T[P] | Gen<T[P]> | Derived<T,T[P]>;
 }
 
-export function val<T>(val : T) : Fact<T> { 
-  return new Fact(() => val);
+export function val<T>(val : T) : Gen<T> { 
+  return new Gen(() => val);
 } 
 
-export function each<T>(f: (seqNum:number) => T) : Fact<T> { 
-  return new Fact(f);
+export function each<T>(f: (seqNum:number) => T) : Gen<T> { 
+  return new Gen(f);
 } 
 
-function buildBase<T>(seqNum : number, builder: Builder<T>) : T {
+interface BaseDerived {
+  derived: Function
+  key: string
+}
+
+interface BaseBuild<T> {
+  readonly value : T
+  readonly derived: BaseDerived[]
+}
+
+function buildBase<T>(seqNum : number, builder: Builder<T>) : BaseBuild<T> {
   const t : {[key: string]: any } = {};
   const keys = Object.getOwnPropertyNames(builder);
-  for (var key of keys) {
+  const derived : BaseDerived[] = [];
+  for (const key of keys) {
     const v = (builder as any)[key];
-    const value = (!!v && typeof v === 'object' && v.constructor === Fact) ? v.build(seqNum) : v;
+    let value = v;
+    if (!!v && typeof v === 'object') {
+      if (v.constructor === Gen) {
+        value = v.build(seqNum);
+      }
+      else if (v.constructor == Derived) {
+        derived.push({key, derived: v});
+      }
+    } 
     t[key] = value;
   }
-  return t as T;
+  return { value: t as T, derived };
 }
 
-export function makeFactory<T>(builder: Builder<T>) : FactoryFunc<T> {
-  let seqNum = 0;
-  return (vals:Partial<T>) => {
-    seqNum++;
-    const v = buildBase(seqNum, builder);
-    return Object.assign({}, v, vals);
-  };
+export function makeFactory<T>(builder: Builder<T>) : Factory<T> {
+  return new Factory(builder);
 }
